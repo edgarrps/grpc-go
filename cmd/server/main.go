2277b14668da383e9e547aa053d57021945b2519
Edgar Santos <edgarrps@gmail.com>
2023-05-07 18:59:42 -0400

    updated main func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
 
 	"github.com/edgarrps/grpc-go/pb"
+	"google.golang.org/grpc"
 )
 
 type Server struct {
@@ -15,5 +18,16 @@ func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 }
 
 func main() {
+	println("Running gRPC server")
 
+	listener, err := net.Listen("tcp", "localhost:9000")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &Server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
