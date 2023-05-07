b892590f881e18225b9c27844bbe730fc296c934
Edgar Santos <edgarrps@gmail.com>
2023-05-07 18:47:49 -0400

    func main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/edgarrps/grpc-go/pb"
+import (
+	"context"
+
+	"github.com/edgarrps/grpc-go/pb"
+)
 
 type Server struct {
 	pb.UnimplementedHelloServer
+}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello, " + in.GetName()}, nil
+}
+
+func main() {
+
 }
