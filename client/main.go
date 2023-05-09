a4de520128273a546cea90c5b19c70e1ddc5638a
edgarrps <edgarrps@gmail.com>
2023-05-08 22:46:07 -0400

    func main

--- client/main.go
+++ client/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+}
