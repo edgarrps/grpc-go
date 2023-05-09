1cf807d2a3b9a38b24cc649e70f7e1c10782cade
edgarrps <edgarrps@gmail.com>
2023-05-08 22:47:04 -0400

    condicional for func main

--- client/main.go
+++ client/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	conn, err := grpc.Dial("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
